24c9cc41749cc5eba92996ed192aa9e82450f6d6
Jobanny Zúñiga <jobannyz@gmail.com>
2021-04-08 18:52:13 +0000

    Enviado para revision

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse_generator").innerHTML = excuse_generator();
+  });
+  function excuse_generator() {
+    let who = ["My dog", "The cat", "The Hulk", "A Dinasour", "The president"];
+    let what_did = ["ate", "stole", "hid", "smashed", "burned"];
+    let the_object = [
+      "the homework",
+      "my food",
+      "the newspaper",
+      "my phone",
+      "the computer"
+    ];
+    let when = [
+      "yesterday",
+      "last night",
+      "a month ago",
+      "a week ago",
+      "last weekend"
+    ];
+
+    let who_position = Math.floor(Math.random() * 5);
+    let what_did_position = Math.floor(Math.random() * 5);
+    let the_object_position = Math.floor(Math.random() * 5);
+    let when_position = Math.floor(Math.random() * 5);
+
+    return (
+      who[who_position] +
+      " " +
+      what_did[what_did_position] +
+      " " +
+      the_object[the_object_position] +
+      " " +
+      when[when_position]
+    );
+  }
 };
